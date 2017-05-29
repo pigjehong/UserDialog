a7ebb93703b39571d11ca3027183876d0cefc42a
appcrate38 <appcrate38@appcrate38ui-iMac.local>
2017-05-29 20:55:36 +0900

    #1 name and email

--- app/src/main/java/com/example/appcrate38/userdialog/MainActivity.java
+++ app/src/main/java/com/example/appcrate38/userdialog/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.appcrate38.userdialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but=(Button)findViewById(R.id.but);
+        but.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        View dlgV=View.inflate(getApplicationContext(), R.layout.dialog, null);
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        setTitle("Input User Information");
+        dialog.setIcon(R.drawable.ryujehong2);
+        dialog.setView(dlgV);
+        dialog.setPositiveButton("OK",null);
+        dialog.setNegativeButton("CANCEL",null);
+        dialog.show();
     }
 }
